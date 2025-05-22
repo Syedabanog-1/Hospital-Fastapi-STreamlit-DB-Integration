04dfeeaee95d76b350a69f0327b48eae42b749b4
syedabanog-1 <gulzar_bano@yahoo.com>
2025-05-22 18:38:15 +0500

    add commit

--- main.py
+++ main.py
@@ -1,42 +1,11 @@
 from fastapi import FastAPI, HTTPException, Depends
-from pydantic import BaseModel
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, Session
-
-DATABASE_URL = "sqlite:///./hospital.db"
-
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
-Base = declarative_base()
-SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+from sqlalchemy.orm import Session
+from module import (
+    DoctorDB, PatientDB, Doctor, Patient,
+    SessionLocal
+)
 
 app = FastAPI()
-
-# === SQLAlchemy Models ===
-class DoctorDB(Base):
-    __tablename__ = "doctors"
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
-    specialty = Column(String)
-
-class PatientDB(Base):
-    __tablename__ = "patients"
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
-    disease = Column(String)
-
-Base.metadata.create_all(bind=engine)
-
-# === Pydantic Models ===
-class Doctor(BaseModel):
-    id: int
-    name: str
-    specialty: str
-
-class Patient(BaseModel):
-    id: int
-    name: str
-    disease: str
 
 # === Dependency ===
 def get_db():
